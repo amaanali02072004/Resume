665101a76785f43013be78c49ebaaecc0c5ed02c
Amaan-Ali-Prodige <amaan.ali@prodigedigital.com>
2023-03-27 13:43:40 +0530

    updated min0r change

--- branch.bat
+++ branch.bat
@@ -1,32 +1,24 @@
 @echo off
 cls
-@REM Color 0C
 title GitHub Pushing
-set /p COM=Commit message: 
+set /p COM=Commit message:
 if defined COM (echo Commit is defined, proceeding...) else (echo [91mCommit is not defined, exiting...[0m && exit)
-set /p BR=Branch Name: 
+set /p BR=Branch Name:
 if defined BR (echo Branch Name is defined) else (echo [91mBranch Name is not defined, exiting...[0m && exit)
-set hyp=----------------------------------------------------------------------------------------
+git branch -m %BR%
 git add *
 git commit -m "%COM%"
 git push origin %BR%
-
-@echo %hyp%
-
-git status
-
-@echo %hyp%
-
-Color 0A
-@echo Pushed and Committed succesfully [%COM%]
-
-@echo %hyp%
+@REM ------
+git checkout main
+@REM explorer "https://bitbucket.org/sulaman/ats-react/pull-requests/new?source=%BR%&t=1"
 git pull origin main
+
 setlocal
 :PROMPT
 SET /P EXIT=Exit? (y/n)
 IF /I "%EXIT%" NEQ "Y" GOTO EEND
-
+git pull origin main
 exit
 :EEND
 branch
